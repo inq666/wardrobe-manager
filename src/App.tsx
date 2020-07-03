fc2e59a4c31f1b722a8ddb4e05d31011eafc47a1
Yahor Neverouski <hardstyle6537@gmail.com>
2020-07-03 23:43:21 +0300

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
+import CollectOutfit from './components/СollectOutfit';
+import AllClothes from './components/AllClothes';
+import HomePage from './components/HomePage';
+import Outfits from './components/Outfits';
 
 const App: React.FC = () => {
   return (
-    <Navbar />
+    <React.Fragment>
+      <Navbar />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/allClothes" component={AllClothes} />
+      <Route path="/collectOutfit" component={CollectOutfit} />
+      <Route path="/outfits" component={Outfits} />
+    </React.Fragment>
   );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <div className="container">
-        <a href="#!" className="brand-logo center">Wardrobe Manager</a>
-        <ul className="left hide-on-med-and-down">
-          <li><a href="sass.html">All clothes</a></li>
-          <li><a href="badges.html">Сollect outfit</a></li>
-          <li><a href="collapsible.html">Add clothes</a></li>
+    <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+      <NavLink to="/" className="navbar-brand">Wardrobe Manager</NavLink>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <li><NavLink className="nav-link" to="/allClothes">All clothes</NavLink></li>
+          </li>
+          <li className="nav-item">
+            <li><NavLink className="nav-link" to="/collectOutfit">Сollect outfit</NavLink></li>
+          </li>
+          <li className="nav-item">
+            <li><NavLink className="nav-link" to="/outfits">Outfits</NavLink></li>
+          </li>
         </ul>
       </div>
     </nav>
+    /*     <nav>
+          <div classNameName="nav-wrapper">
+
+            <ul classNameName="left hide-on-med-and-down">
+
+
+
+            </ul>
+          </div>
+        </nav> */
   );
 };
 
